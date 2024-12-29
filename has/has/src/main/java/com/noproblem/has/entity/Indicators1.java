125e5b938c1d4533821938ca265ab462f9135a59
尹诗兰 <11428700+ysl111@user.noreply.gitee.com>
2024-12-29 12:32:54 +0800

    根据项目名和一级指标名查询一级指标得分

---
+++ has/has/src/main/java/com/noproblem/has/entity/Indicators1.java
@@ -0,0 +1,16 @@
+package com.noproblem.has.entity;
+
+import lombok.Data;
+
+/**
+ * @Author：ysl
+ * @Package：com.noproblem.openrank.entity
+ * @Project：openRank
+ * @name：Indicators1
+ * @Date：2024/12/29 10:46
+ */
+@Data
+public class Indicators1 {
+    private int indicator1_id;
+    private String indicator1_name;
+}
---
+++ has/has/src/test/java/com/noproblem/has/HasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noproblem.has;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
